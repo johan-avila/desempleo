fa705d6b1f751a54c1d50cdad76d7fdc11ebf9e0
johan-avila <johan-avila@outlook.com>
2020-05-06 01:40:37 -0500

    props 20%

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -8,11 +8,19 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./style-components/card.css"
 class Card extends React.Component {
 
+/* props
+
+this.props.linkTo //a donde va el card
+this.props.img    //img del card
+this.props.altImg // alt del img
+this.props.Title  //titulo del card
+*/
+
 
     render() {
         return (
             <React.Fragment  >
-                <Link className="customStateReset" to={"/general"}>
+                <Link className="customStateReset" to={this.props.linkTo}>
                     <div className="col-3mb-4 customGlobalCard">
                         <div className="card h-100 customCardBody ">
                             <img src={this.props.img} className="card-img-top " alt={this.props.altImg} />
--- src/componentes/cardMenu.jsx
+++ src/componentes/cardMenu.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import {Link }from "react-router-dom"
 import "./style-components/cardMenu.css"
 class CardMenu extends React.Component {
+/* props
+this.props.cardMenuTitle // Autoexplicativo
+
+*/
+
+
+
     render() {
         return (
             <React.Fragment>
                 <div className="card text-white Tarjeta-menu mb-3 col-2  customCardBodyMenu" >
-                    <h3 className="card-header">Header</h3>
+        <h3 className="card-header">{this.props.cardMenuTitle}</h3>
                     <div className="card-body">
                         <ul>
-                            <li><Link className="hover-anclas" href="/">hola</Link></li>
+                            <li><Link className="hover-anclas" to={"/general"}>hola</Link></li>
                             <li><Link className="hover-anclas" href="/">bba</Link></li>
                             <li><Link className="hover-anclas" href="/">como</Link></li>
                             <li><Link className="hover-anclas" href="/">tu</Link></li>
