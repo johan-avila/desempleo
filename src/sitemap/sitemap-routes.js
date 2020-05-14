f72a6e1a99958694bc4d43debf63b33da3c78a63
johan-avila <johan-avila@outlook.com>
2020-05-14 03:54:53 -0500

    COM4 am

---
+++ src/sitemap/sitemap-routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route,Switch } from 'react-router';
+ 
+export default (
+        <Switch>    
+            <Route exact path="/" />
+            <Route exact path="/generalLayout"  />
+            <Route exact path="/apoyos"  />
+            <Route exact path="/creditos-rapidos"  />
+        </Switch>
+
+
+);
